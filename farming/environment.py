6709ebeb4b1365d23391e2288a49f306d354e969
b5050d <bdyer5280@gmail.com>
2025-07-05 08:27:06 -0600

    trying to merge

---
+++ farming/environment.py
@@ -0,0 +1,19 @@
+
+import pygame
+import numpy as np
+
+
+class Environment:
+    def __init__(self):
+        self.reset()
+
+
+    def reset(self):
+        self.player_position = 
+
+
+    def pass
+
+    def step(self):
+        pass
+
--- timetracking.py
+++ timetracking.py
@@ -5,4 +5,6 @@ values = [
     ("20250701", "60", "Working on the Beginner Game"),
     ("20250702", "25", "Starting to actually train the AI for the beginner game and its slow as fuck!"),
     ("20250703", "17", "Starting to actually train the AI for the beginner game and its slow as fuck!"),
+
+    ("20250705", "20", "Thinking about building the next simulation"),
 ]
